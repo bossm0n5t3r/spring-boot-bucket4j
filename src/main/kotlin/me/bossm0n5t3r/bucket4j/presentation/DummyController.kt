1af1be83ade302f9d00fe442fe3dd49122d8c9a6
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-11-06 03:24:10 +0900

    feat: Add DummyController

---
+++ src/main/kotlin/me/bossm0n5t3r/bucket4j/presentation/DummyController.kt
@@ -0,0 +1,15 @@
+package me.bossm0n5t3r.bucket4j.presentation
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
+
+@RestController
+@RequestMapping("/")
+class DummyController {
+    @GetMapping
+    fun healthCheck(): Mono<ResponseEntity<Unit>> = ResponseEntity.ok().build<Unit>().toMono()
+}
