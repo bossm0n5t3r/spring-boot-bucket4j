0398a57985568498d8a15c16a907dbebcebffeac
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-11-16 22:00:00 +0900

    feat: Add PricingPlan

---
+++ src/main/kotlin/me/bossm0n5t3r/bucket4j/enumerated/PricingPlan.kt
@@ -0,0 +1,37 @@
+package me.bossm0n5t3r.bucket4j.enumerated
+
+import io.github.bucket4j.Bandwidth
+import java.time.Duration
+
+enum class PricingPlan(val limit: Bandwidth) {
+    FREE(
+        Bandwidth.builder()
+            .capacity(20)
+            .refillIntervally(20, Duration.ofHours(1))
+            .build(),
+    ),
+    BASIC(
+        Bandwidth.builder()
+            .capacity(40)
+            .refillIntervally(40, Duration.ofHours(1))
+            .build(),
+    ),
+    PROFESSIONAL(
+        Bandwidth.builder()
+            .capacity(100)
+            .refillIntervally(100, Duration.ofHours(1))
+            .build(),
+    ),
+    ;
+
+    companion object {
+        fun resolvePlanFromUserRole(userRole: UserRole): PricingPlan {
+            return when (userRole) {
+                UserRole.ANONYMOUS -> FREE
+                UserRole.USER -> BASIC
+                UserRole.PREMIUM -> PROFESSIONAL
+                UserRole.ADMIN -> PROFESSIONAL
+            }
+        }
+    }
+}
